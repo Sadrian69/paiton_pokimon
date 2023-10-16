aacaf7ae656f99b9070a2e178f0652c2c0bf2a78
Sadrian <adriansteven92@gmail.com>
2023-10-17 03:19:46 +0700

    KELAR

--- main.py
+++ main.py
@@ -25,120 +25,128 @@ for i in choices.index:
 valInput = input()
 wm.loc[len(wm)] = pd.Series({'attribute' : questions['attribute'][query], 'value' : valInput})
 
-for i in wm.index:
-    print(wm['attribute'][i],': ',wm['value'][i])
 
 stop = False
 
 while not stop:
+    
+    print()
+    print("Working Memory")
+    for i in wm.index:
+        print(wm['attribute'][i],': ',wm['value'][i])
+    print()
+        
     # step 3
     # cek 'Active'
-    for i in rules['A']:
-        actFound = False
-        if i == 1:
+    actFound = False
+    for i in rules.index:
+        if rules['A'][i] == '1':
             actFound = True
             break
+        
     if not actFound:
-        print("Gagal di step 3")
+        print("Pokemon tidak ditemukan")
         stop = True
         break
         
     # perubahan premise
-    td = False
     for i in rules.index: # untuk semua rule
-        if rules['A'][i] == 1:
+        # print('rule', rules['id'][i])
+        if rules['A'][i] == '1':
             for j in ruleDetails.index: # cari premis
-                if ruleDetails['rule_id'][j] == i:
-                    preID = ruleDetails['premise_id'][j]
-                    if premiseTable['attribute'][preID+1] == questions['attribute'][query]: # kalo sama dg query
-                        if premiseTable['value'][preID+1] == valInput: # dan bener
-                            premiseTable['premise clause status'][preID+1] = 'TU'
+                if ruleDetails['rule_id'][j] == rules['id'][i]:
+                    preID = int(ruleDetails['premise_id'][j])-1
+                    
+                    # print(premiseTable['attribute'][int(preID)], premiseTable['value'][int(preID)])
+                    if premiseTable['attribute'][preID] == wm['attribute'][len(wm)-1]: # kalo sama dg query
+                        if premiseTable['value'][preID] == wm['value'][len(wm)-1]: # dan bener
+                            premiseTable['premise clause status'][int(preID)] = 'TU'
+                            # print(premiseTable['value'][int(preID)],'TU')
                         else: # tapi salah
                             # step 3a
-                            premiseTable['premise clause status'][preID+1] = 'FA'
+                            premiseTable['premise clause status'][int(preID)] = 'FA'
+                            # print(premiseTable['value'][int(preID)],'FA')
                             rules['A'][i] = 0
                             rules['D'][i] = 1
                         break
-                    
-# note to self
-# nanti waktu question if the question type is i-conclude langsung auto masuk
-# tapi masuknya kalo udah TD
-# trus kalo support yang TD langsung crot (dipikir nanti)
 
-#         # step 3b
-#         if pcs[i] == ['TU' for x in range(len(pcs[i]))]: # semua premis benar
-#             rs[i].append('TD')
-#             # print("harusnya ini ", poke['nama'][i])
-#             td = True
-#     # step 3c
-#     if td:
-#         # step 4
-#         if len(aq) > 0:
-#             del aq[0] # cross out topmost attr
-#         for i in range(len(pcs)): # change status of rule 
-#             if 'TD' in rs[i]:
-#                 # print("harusnya ini ", poke['nama'][i])
-#                 rs[i].remove('TD')
-#                 rs[i].append('FD')
-#                 wm.append(['conclusion', i]) # conclusion at bottom of wm
-#         stop = True
-#         continue # return to 3
-#     else:
-#         # step 5
-#         if len(aq) > 0:
-#             del aq[0] # cross out topmost attr
-#         # step 6
-#         recent = -1 # recently marked rule
-#         unmarked = False # sudah pernah jalanin rule 8
-#         for i in range(len(rs)): # scan for unmarked active
-#             if unmarked:
-#                 break
-#             if 'U' in rs[i] and 'A' in rs[i]:
-#                 rs[i].remove('U')
-#                 rs[i].append('M') # mark the first one
-#                 recent = i
-#                 # step 7
-#                 for j in range(len(pcs[recent])):
-#                     if pcs[recent][j] == 'FR': # query user for value
-#                         query = poke['attr'][recent][j][0]
-#                         print(prem[query], "?")
-#                         for k in choices[query]:
-#                             print(k)
-#                         valInput = input()
-#                         # cek keberadaan itu di yang masih aktif
-#                         valid = False
-#                         if valInput in choices[query]:
-#                             for i in range(len(rs)):
-#                                 if 'A' in rs[i]:
-#                                     # print("test1", poke['nama'][i], query, valInput)
-#                                     if [query, choices[query].index(valInput)] in poke['attr'][i]:
-#                                         valid = True
-                        
-#                         while valInput not in choices[query] or not valid: # if no response atau gaada
-#                             print("Tidak menemukan data mohon coba lagi")
-#                             valInput = input()
-#                             # cek keberadaan itu di yang masih aktif
-#                             if valInput in choices[query]:
-#                                 for i in range(len(rs)):
-#                                     if 'A' in rs[i]:
-#                                         # print("test2", poke['nama'][i], query, valInput)
-#                                         if [query, choices[query].index(valInput)] in poke['attr'][i]:
-#                                             valid = True
+    # step 3b
+    for i in rules.index: 
+        if rules['TD'][i] == '1' or rules['FD'][i] == '1':
+            continue
+        td = True
+        for j in ruleDetails.index:
+            if ruleDetails['rule_id'][j] == rules['id'][i]:
+                preID = int(ruleDetails['premise_id'][j])-1
+                if premiseTable['premise clause status'][preID] != 'TU':
+                    # ada premis yang salah
+                    td = False
+                    
+        if td:
+            rules['TD'][i] = '1'
+            break
+    
+    # step 3c
+    if td:
+        # step 4
+        for i in rules.index: # change status of rule 
+            if rules['TD'][i] == '1':
+                # print('TD', i+1)
+                rules['TD'][i] = '0'
+                rules['FD'][i] = '1'
+                wm.loc[len(wm)] = pd.Series({'attribute' : rules['attribute_conclude'][i] , 'value' : rules['conclusion'][i]}) # conclusion at bottom of wm
+                if rules['attribute_conclude'][i] == 'support':
+                    stop = True
+        continue # return to 3
+    else:
+        # step 6
+        unmarked = False # sudah pernah jalanin rule 8
+        
+        for i in rules.index: # scan for unmarked active
+            if unmarked:
+                break
+            if rules['A'][i] == '1' and rules['U'][i] == '1':
+                rules['U'][i] = '0'
+                rules['M'][i] = '1' # mark the first one
+                
+                # print('UA', rules['id'][i], rules['U'][i], rules['M'][i])
+                
+                # step 7
+                for j in ruleDetails.index:
+                    preID = int(ruleDetails['premise_id'][j])-1
+                    if ruleDetails['rule_id'][j] == rules['id'][i] and premiseTable['premise clause status'][int(preID)] == 'FR':
+                        # query user 
+                        curQuestionAttr = premiseTable['attribute'][int(preID)]
+                        idQuestion = ''
+                        for k in questions.index:
+                            if questions['attribute'][k] == curQuestionAttr:
+                                idQuestion = k
+                                break
+                        query = idQuestion
                             
-#                         unmarked = True
-#                         value = choices[query].index(valInput)
-#                         # step 8
-#                         aq.append(query)
-#                         wm.append([query,value])
-#                         rs[recent].remove('M')
-#                         rs[recent].append('U')
-#                         break
-#         # step 6 cont
-#         if not unmarked: # no such rules can be found
-#             print("Gagal di step 6")
-#             stop = True
-#             break
-# # sudah selesai, jawaban di wm
-# for i in wm:
-#     if i[0] == 'conclusion':
-#         print(poke['nama'][i[1]])
+                        if questions['type'][idQuestion] == 'question':
+                            print(questions['question'][query])
+                            for k in choices.index:
+                                if choices['id_question'][k] == questions['id'][query]:
+                                    print(choices['choice'][k])
+                            valInput = input()
+                           
+                            # step 8
+                            wm.loc[len(wm)] = pd.Series({'attribute' : questions['attribute'][query], 'value' : valInput})
+                    
+                        unmarked = True
+                        break
+                    
+                rules['U'][i] = '1'
+                rules['M'][i] = '0'
+                        
+        # step 6 cont
+        if not unmarked: # no such rules can be found
+            print("Pokemon tidak ditemukan")
+            stop = True
+            break
+
+# sudah selesai, jawaban di wm
+for i in wm.index:
+    if wm['attribute'][i] == ans:
+        print('Pokemon yang anda cari adalah', wm['value'][i])
